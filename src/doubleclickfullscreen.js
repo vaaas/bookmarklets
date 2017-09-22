8275ee97211bfcde5a4e232a766ccd64a1cfc8b1
Vasileios Pasialiokis <whiterocket@outlook.com>
2017-09-22 17:46:35 +0300

    small style changes

--- src/doubleclickfullscreen.js
+++ src/doubleclickfullscreen.js
@@ -1,3 +1,6 @@
 (function() {
-document.body.ondblclick = function() { this.mozRequestFullScreen() }
+var container = document.getElementById("container")
+container.ondblclick = function() { this.mozRequestFullScreen() }
+container.style.overflow = "auto"
+container.style.padding = "0.5em"
 })()
