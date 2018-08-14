59dee3ffbff973e4f61b3abafdfbf8af21c7b8f6
vas <whiterocket@outlook.com>
2018-08-14 21:58:14 +0300

    Update cookieclicker.js

--- src/cookieclicker.js
+++ src/cookieclicker.js
@@ -1,10 +1,13 @@
 (function() {
 	var e = document.createEvent("HTMLEvents")
 	e.initEvent("click", true, true)
-	var a = document.getElementById("bigCookie")
-	setInterval(function() { a.dispatchEvent(e) }, 20)
+	var big = document.querySelector("#bigCookie")
+	setInterval(function() { big.dispatchEvent(e) }, 20)
 	setInterval(function() {
-		var a = document.getElementsByClassName("shimmer")
-		if (a !== undefined) for (var i = 0; i < a.length; i++) a[i].dispatchEvent(e)
+		var a = document.querySelectorAll(".shimmer")
+		if (a !== undefined) for (let i = a.length - 1; i >= 0; i--) a[i].dispatchEvent(e)
 	}, 1000)
+	setInterval(function() {
+		Game.wrinklers.forEach(wrinkler => wrinkler.hp = 0)
+	}, 10000)
 })()
